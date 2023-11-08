fff88ee4f68e99d1d56f9b1ecb25bd6fa69a7596
Ana Pavlovic <pavlovicana256@gmail.com>
2023-11-09 00:52:45 +0100

    Dodavanje foldera service i povezivanje sa firebase

---
+++ src/app/service/data.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { collection, collectionData, Firestore, doc,deleteDoc } from '@angular/fire/firestore';
+
+export interface Task{
+  id?:number;
+  name:string;
+  date:string;
+  category:string;
+  done:boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataService {
+
+  constructor(private firestore:Firestore) { }
+  getTasks(){
+    const tasksRef = collection(this.firestore,'tasks');
+    // smesta se referenca do odredjene kolekcije u firebase-u BP
+
+    return collectionData(tasksRef,{idField:'id'});
+    // referenca ka kolekciji, jedinstveno polje
+
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    apiKey: "AIzaSyC9FZLsiPqkzoIoB986u0t13EslB8VsXr0",
+    authDomain: "aplikacija-5674f.firebaseapp.com",
+    projectId: "aplikacija-5674f",
+    storageBucket: "aplikacija-5674f.appspot.com",
+    messagingSenderId: "994898325438",
+    appId: "1:994898325438:web:9ba5ec5afd809b0c37d3ef",
+    measurementId: "G-Z5LYBLN6JM"
+  },
   production: false
-};
-
+}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
